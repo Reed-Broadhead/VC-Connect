7c4ee6f7cc6442836840495f56bdae5bf55c735e
Franco Lepe <Francolepe169@gmail.com>
2024-04-04 23:00:46 -0400

    copy meeting button fix

--- Front-End/src/components/JoinMeeting.tsx
+++ Front-End/src/components/JoinMeeting.tsx
@@ -1,30 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from "react";
 import axios, { Axios } from "axios"
-import { DyteMeeting } from '@dytesdk/react-ui-kit';
-import { useDyteClient } from '@dytesdk/react-web-core';
+import { DyteMeeting } from "@dytesdk/react-ui-kit";
+import { useDyteClient, DyteProvider } from "@dytesdk/react-web-core";
 
 export default function JoinMeeting() {
-    const [meeting, initMeeting] = useDyteClient();
+  const [meeting, initMeeting] = useDyteClient();
 
-    useEffect(() => {
-      const searchParams = new URL(window.location.href).searchParams;
-  
-      const authToken = searchParams.get('authToken');
-  
-      if (!authToken) {
-        alert(
-          "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
-        );
-        return;
-      }
-  
-      initMeeting({
-        authToken,
-      });
-    }, []);
-  
-    // By default this component will cover the entire viewport.
-    // To avoid that and to make it fill a parent container, pass the prop:
-    // `mode="fill"` to the component.
-    return <DyteMeeting meeting={meeting!} />;
-  }
+  useEffect(() => {
+    const searchParams = new URL(window.location.href).searchParams;
+
+    const authToken = searchParams.get("authToken");
+
+    if (!authToken) {
+      alert(
+        "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
+      );
+      return;
+    }
+
+    initMeeting({
+      authToken,
+    });
+  }, []);
+
+  // By default this component will cover the entire viewport.
+  // To avoid that and to make it fill a parent container, pass the prop:
+  // `mode="fill"` to the component.
+  return (
+    <DyteProvider value={meeting}>
+      <button
+        style={{ position: "absolute", zIndex: 100 }}
+        onClick={() => console.log(meeting.meta.meetingId)}
+      >
+        Copy Meeting ID
+      </button>
+      <DyteMeeting meeting={meeting!} />
+    </DyteProvider>
+  );
+}
